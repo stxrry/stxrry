14cc58261f9a4698d44265dc335ce4ad71206fb6
stxrry <32224462+stxrry@users.noreply.github.com>
2018-09-02 20:29:59 +0800

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,15 @@
-// Wrap every letter in a span
+// comment
+<h1 id="demo"> hello </h1>
+
+<script>
+document.getElementById("demo").onmouseover = function() {mouseOver()};
+document.getElementById("demo").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+    document.getElementById("demo").style.color = "rgb(255, 0, 167)";
+}
+
+function mouseOut() {
+    document.getElementById("demo").style.color = "rgb(128, 0, 196)";
+}
+</script>
