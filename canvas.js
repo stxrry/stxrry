2834ea65cc7aff8244bf33c3852d4dc8f9cd9bd5
Stxrry <jaapscj@gmail.com>
2018-09-05 01:38:27 +0800

    Add Canvas Content

--- canvas.js
+++ canvas.js
@@ -8,8 +8,17 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
+c.fillStyle = 'rgba(255, 0, 0, 0.1)';
 c.fillRect(100, 100, 100, 100);
 c.fillRect(400, 100, 100, 100);
 c.fillRect(300, 300, 100, 100);
 
 console.log(canvas);
+
+// Line
+c.beginPath();
+c.moveTo(50, 300);
+c.lineTo(300, 100);
+c.lineTo(400, 300);
+c.strokeStyle = "#fa34a4";
+c.Stroke();
