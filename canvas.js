01766aa63f9b3bd71cc5c5ecf78880c520ae4894
Stxrry <jaapscj@gmail.com>
2018-09-05 01:07:23 +0800

    New Canvas

--- canvas.js
+++ canvas.js
@@ -1,1 +1,15 @@
-canvas.js
+console.log('welcome to coding js');
+
+var canvas = document.querySelector('canvas')
+;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+c.fillRect(100, 100, 100, 100);
+c.fillRect(400, 100, 100, 100);
+c.fillRect(300, 300, 100, 100);
+
+console.log(canvas);
